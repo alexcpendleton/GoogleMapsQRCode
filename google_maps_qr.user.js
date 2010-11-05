bd9294fb73f6ddfa88aa46a68467c050f346b458
Alex Pendleton <alex.c.pendleton@gmail.com>
2010-11-05 11:17:39 -0400

    Had to change the @include to remove the trailing slash for Chrome and perhaps other browsers

--- google_maps_qr.user.js
+++ google_maps_qr.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Google Maps QR
 // @namespace      http://userscripts.org/users/120060
-// @include        http://maps.google.com/*
+// @include        http://maps.google.com*
 // @description    Generates a QR code for the current map for scanning to your phone or other device.
 // ==/UserScript==
 
